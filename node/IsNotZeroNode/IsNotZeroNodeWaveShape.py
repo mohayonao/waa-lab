7641a646998fa5c03da9cadbd8d8c5d307806057
mohayonao <mohayonao@gmail.com>
2016-02-23 20:08:17 +0900

    Update: IsNotZeroNode

---
+++ node/IsNotZeroNode/IsNotZeroNodeWaveShape.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-1.0, 1.0, 0.0001)
+
+ws1 = np.zeros_like(x)
+ws2 = np.zeros_like(x)
+
+ws1[:] = 1
+ws1[np.abs(x) < 1e-4] = 0
+
+ws2[x > 0.01] = 1
+
+plt.subplot(211)
+plt.title("ws1")
+plt.xlim(-1, 1)
+plt.ylim(-1.2, +1.2)
+plt.plot(x, ws1)
+
+plt.subplot(212)
+plt.title("ws2")
+plt.xlim(-1, 1)
+plt.ylim(-1.2, +1.2)
+plt.plot(x, ws2)
+
+plt.show()
